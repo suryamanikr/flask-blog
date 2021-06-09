415275b157fc5924a5e3b23a3058079e3253b2e0
suryamanikr <66545094+suryamanikr@users.noreply.github.com>
2021-06-09 07:56:32 +0530

    Add files via upload
    This is the first commit and in this project only contact form is working.

---
+++ Coding Thunder/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    '''
+    sno, name phone_num, msg, date, email
+    '''
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
+
+db.create_all()
+db.session.commit()
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/post")
+def post():
+    return render_template('post.html')
+
+
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    if(request.method == 'POST'):
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone_num = request.form.get('phone_num')
+        msg = request.form.get('msg')
+        entry = Contacts(name=name, email=email, phone_num=phone_num,
+                         msg=msg, date=datetime.now(),)
+        db.session.add(entry)
+        db.session.commit()
+
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
